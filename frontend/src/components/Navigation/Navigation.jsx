41e21038540496a79d53fa2d7033b06a5c056934
Rashid <rnurshinov@gmail.com>
2022-09-09 18:59:15 +0500

    realeaseAmin1.1

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -8,12 +8,12 @@ const Navigation = () => {
     return (  
             <nav class="navbar navbar-dark bg-dark">   
             <div class="container-fluid">
-            <li class="nav-item">
-                <Link to='Main'><button type="button" class="btn btn-secondary">Главная</button></Link>
-            </li>
-            <li class="nav-item dropdown">   
-                <Link to='Shop'><button type="button" class="btn btn-secondary">Товары</button></Link>
-            </li>
+            <li class="nav-item"><Link to='Main'><button type="button" class="btn btn-secondary">Главная</button></Link></li>
+            <li class="nav item"><Link to='Shop'><button type="button" class="btn btn-secondary">Меню</button></Link></li>
+            <li class="nav item"><Link to='/'><button type="button" class="btn btn-secondary">Доставка</button></Link></li>
+            <li class="nav item"><Link to='/'><button type="button" class="btn btn-secondary">Контакты</button></Link></li>
+            <li class="nav item"><Link to='/'><button type="button" class="btn btn-secondary">История нашей Шаурмы</button></Link></li>
+            <li class="nav item"><Link to='/'><button type="button" class="btn btn-secondary">Оплата</button></Link></li>
                 </div> 
             </nav>
     )
